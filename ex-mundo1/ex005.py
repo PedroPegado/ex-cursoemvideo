599c37d1a02821d8805063f09473f740455ada0d
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:26:38 -0300

    Update ex005.py

--- ex-mundo1/ex005.py
+++ ex-mundo1/ex005.py
@@ -1,7 +1,10 @@
 print('\033[1;35m===== EX 005 =====\033[m')
 
+def antesus():
+    print(f'Antecessor:{num-1}')
+    print(f'Sucessor:{num++1}')
+
 num = int(input('Digite um número:'))
 
-print(f'Antecessor:{num-1}')
-print(f'Sucessor:{num++1}')
+antesus()
 
