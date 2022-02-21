86f7c7966d132d870a645d462f32ebf63dad9dfe
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:28:15 -0300

    Update ex006.py

--- ex-mundo1/ex006.py
+++ ex-mundo1/ex006.py
@@ -1,9 +1,10 @@
 print('\033[1;35m===== EX 006 =====\033[m')
 
+def operacoes():
+    print(f'O dobro de {num} é igual a: {num*2}')
+    print(f'O triplo de {num} é igual a: {num*3}')
+    print(f'A raiz quadrada de {num} é igual a: {num**0.5}')
+
 num = int(input('Digite um número: '))
 
-print(f'O dobro de {num} é igual a: {num*2}')
-
-print(f'O triplo de {num} é igual a: {num*3}')
-
-print(f'A raiz quadrada de {num} é igual a: {num**0.5}')
+operacoes()
