2ca4a158ac302506ff7ba0543e53be06953942a5
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:56:32 -0300

    Update ex012.py

--- ex-mundo1/ex012.py
+++ ex-mundo1/ex012.py
@@ -1,7 +1,10 @@
 print('\033[1;35m===== EX 012 =====\033[m')
 
+def desconto():
+    desc = price - price/100*5
+    print(f'O valor do produto com 5% de desconto é {desc:.2f}')
+
+    
 price = float(input('Qual o preço do produto? R$'))
 
-desc = price - price/100*5
-
-print(f'O valor do produto com 5% de desconto é {desc:.2f}')
+desconto()
