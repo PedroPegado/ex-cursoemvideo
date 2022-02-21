0f52d30f45e10dc1504b1f5ad47cf243e230fbca
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:44:38 -0300

    Update ex010.py

--- ex-mundo1/ex010.py
+++ ex-mundo1/ex010.py
@@ -1,6 +1,9 @@
 print('\033[1;35m===== EX 010 =====\033[m')
 
-real = float(input('Quanto você tem na carteira? R$'))
-dol = real/5.33
+def dolar():
+    dol = real/5.09
+    print(f'Com esse valor de R${real}(REAL), você pode adquirir USD{dol:.3}(DÓLAR)')
 
-print(f'Com esse valor de R${real}(REAL), você pode adquirir USD{dol:.3}(DÓLAR)')
+real = float(input('Quanto você tem na carteira? R$'))
+
+dolar()
