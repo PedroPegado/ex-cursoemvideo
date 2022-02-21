6fcbc845894f01f5ee48feeee3b1f0cae63a2c08
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:57:40 -0300

    Update ex013.py

--- ex-mundo1/ex013.py
+++ ex-mundo1/ex013.py
@@ -1,7 +1,9 @@
 print('\033[1;35m===== EX 013 =====\033[m')
 
+def aumento():
+    aumento = salario ++ salario/100*15
+    print(f'Após o aumento de 15%, seu salário será de R${aumento:.2f}')
+
+
 salario = float(input('Qual o seu salário atual? '))
-
-aumento = salario ++ salario/100*15
-
-print(f'Após o aumento de 15%, seu salário será de R${aumento:.2f}')
+aumento()
