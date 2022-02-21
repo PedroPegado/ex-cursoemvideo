112a227c2ae4f65821f9a47157162b8c2a25f444
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-02-21 14:29:43 -0300

    Update ex007.py

--- ex-mundo1/ex007.py
+++ ex-mundo1/ex007.py
@@ -1,9 +1,12 @@
 print('\033[1;35m===== EX 007 =====\033[m')
 
-x= float(input('\033[7;40mDigite a primeira nota\033[m:'))
-y= float(input('\033[7;40mDigite a segunda nota\033[m:'))
+def nota():
+    s = x+y
+    m = s/2
+    print(f'A soma das suas notas foi {s}, e a sua média foi {m}.')
 
-s = x+y
-m = s/2
 
-print(f'A soma das suas notas foi {s}, e a sua média foi {m}.')
+x = float(input('\033[7;40mDigite a primeira nota\033[m:'))
+y = float(input('\033[7;40mDigite a segunda nota\033[m:'))
+
+nota()
