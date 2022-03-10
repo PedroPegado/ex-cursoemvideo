71980a2b1ea7f76b5170a49b9d9768b18486def6
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-03-10 14:32:07 -0300

    Update ex906.py

--- ex-999/ex906.py
+++ ex-999/ex906.py
@@ -2,3 +2,14 @@ print('='*50)
 print('CALCULO SALARIAL'.center(50))
 print('='*50)
 
+linha = '=' * 50
+
+def salario():
+    ganho_hora = float(input('\nDigite o quanto você ganha por hora: '))
+    horas = int(input('\nDigite as horas que você trabalhou no mês: '))
+    print(f'\n{linha}')
+    salariof = ganho_hora * horas
+    print(f'\n\033[1;32m         Seu salário será de {salariof:.2f}R$\033[m')
+    print(f'\n{linha}')
+salario()
+
