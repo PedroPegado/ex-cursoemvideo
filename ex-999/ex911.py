709dc505ea6cce9912103e25dbf5193bd38744c0
Pedro Pegado <93950768+PedroPegado@users.noreply.github.com>
2022-03-17 14:53:54 -0300

    ex911

--- ex-999/ex911.py
+++ ex-999/ex911.py
@@ -9,10 +9,19 @@ from traceback import print_tb
 def tintas():
     metros = float(input('Digite o tamanho da sua parede em metros quadrados(m²): '))
     litros = metros/3
-    latas = math.ceil(litros / 18)
-    galoes = math.ceil(litros / 3.6)
+    latas = round(litros / 18)
+    galoes = round(litros / 3.6)
     preco_lata = latas * 80
     preco_galoes = galoes * 25
+    latas_mistas18 = ((litros * 0.10) + litros) / 18
+    litros18 = math.trunc(latas_mistas18) * 18
+    resto18 = ((litros * 0.10) + litros) - litros18
+    latas_mistas36 = resto18 / 3.6
+    misto_todo = math.trunc(latas_mistas18) + math.ceil(latas_mistas36)
+    misto_preco18 = math.trunc(latas_mistas18) * 80
+    misto_preco36 = math.ceil(latas_mistas36) * 25
+    total_mistos = misto_preco18 + misto_preco36
+     
     while True:
         print(linha)
         escolha = int(input('''\nESCOLHA A FORMA QUE DESEJA COMPRAR:
@@ -26,21 +35,24 @@ Digite o número da sua escolha: '''))
         print(linha)
         if escolha == 1:
             print(f'''QUANTIDADE DE LATAS: {latas}
-            PREÇO: {preco_lata}R$''')
+PREÇO: {preco_lata}R$''')
             
         elif escolha == 2:
             print(f'''QUANTIDADE DE LATAS: {galoes}
-            PREÇO: {preco_galoes}R$''')
+PREÇO: {preco_galoes}R$''')
         
         elif escolha == 3:
-            
+            print(f'''LATAS DE 18 LITROS = {math.trunc(latas_mistas18)}
+GALÕES DE 3,6 LITROS: {math.ceil(latas_mistas36)}
+VALOR TOTAL(com 10% de quebra): {total_mistos}''')
 
         elif escolha == 4:
             break
-
+        print(linha)
         sair = int(input('''
         [ 1 ] ESCOLHER OUTRA OPÇÃO
-        [ 2 ] SAIR DO PROGRAMA'''))
+        [ 2 ] SAIR DO PROGRAMA
+        Opção: '''))
         if sair == 1:
             continue
         else:
